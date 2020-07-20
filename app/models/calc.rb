2d660f9fd0e5bd90263562f7dd8c44c43cb0ebad
Victor <viktor.markevich@faceit.com.ua>
2020-07-20 08:54:06 +0300

    Remove not used views; Add calc styles

--- app/models/calc.rb
+++ app/models/calc.rb
@@ -2,4 +2,6 @@ class Calc
   include Mongoid::Document
   field :a, type: String
   field :b, type: String
+  field :result, type: String
+  field :action, type: String
 end
--- app/views/calcs/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.partial! "calcs/calc", calc: @calc
