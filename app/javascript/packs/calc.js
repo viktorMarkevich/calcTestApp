6cb7bfdabde743ae5a783ef4c7c7e8f448885fa3
Victor <viktor.markevich@faceit.com.ua>
2020-07-20 15:46:31 +0300

    Add timeout to js

--- app/javascript/packs/calc.js
+++ app/javascript/packs/calc.js
@@ -6,6 +6,8 @@ $( document ).on('turbolinks:load', function() {
 function clickOnAction() {
   $('body').on('click', '.btn-secondary', function(e){
     e.preventDefault();
+    $('.btn-secondary').addClass('btn-disabled');
+
     var operator = $(e.target).data('operator');
     var argA = $('#calc_a').val();
     var argB = $('#calc_b').val();
@@ -17,6 +19,10 @@ function clickOnAction() {
       data: { calc: { operator: operator, a: argA, b: argB } },
       success: function (response) {
         $('#result').val(response.result);
+
+        $('.btn-disabled').removeClass('btn-disabled'); // you can't see the buttons disabling because it is very faster query.
+                                                        // To see it comment string above and uncomment below string
+        // setTimeout(() => {  $('.btn-disabled').removeClass('btn-disabled'); }, 3000);
       },
       error: function (errorResponse) {
         console.log('errorResponse: ', errorResponse);
@@ -41,5 +47,5 @@ function inputValidation() {
 function isInputValid(selector) {
   var inputVal = parseFloat($(selector).val());
 
-  return (Number.isInteger(inputVal) && inputVal > 0 && inputVal < 100)
+  return (Number.isInteger(inputVal) && inputVal > 0 && inputVal < 100);
 }
