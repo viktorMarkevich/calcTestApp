db47a2846dc26f641477f3bb77db39404126a790
Victor <viktor.markevich@faceit.com.ua>
2020-07-21 11:26:56 +0300

    Add specs for all operator types

--- spec/requests/calcs_spec.rb
+++ spec/requests/calcs_spec.rb
@@ -13,15 +13,53 @@ RSpec.describe 'Calcs', type: :request do
   describe 'POST /calcs' do
     let(:headers) {{ 'Content-Type' => 'application/json' }}
 
-    context 'When valid request' do
-      it 'should return 200 status' do
-        post '/calcs.json', params: { calc: { operator: '+', a: 1, b: 3 } }.to_json, headers: headers
-        expect(response).to have_http_status(200)
+    context 'Valid case' do
+      context 'When operator is "+"' do
+        it 'should return 200 status' do
+          post '/calcs.json', params: { calc: { operator: '+', a: 1, b: 3 } }.to_json, headers: headers
+          expect(response).to have_http_status(200)
+        end
+
+        it 'should create a calc object and return json' do
+          post '/calcs.json', params: { calc: { operator: '+', a: 1, b: 3 } }.to_json, headers: headers
+          expect(json).to eq({ 'result' => "Operation: 1 + 3\nResult: 4\nID: #{Calc.last.id}\nCount: 1\n#{'*'*20}" })
+        end
       end
 
-      it 'should create a calc object and return json' do
-        post '/calcs.json', params: { calc: { operator: '+', a: 1, b: 3 } }.to_json, headers: headers
-        expect(json).to eq({ 'result' => "Operation: 1 + 3\nResult: 4\nID: #{Calc.last.id}\nCount: 1\n#{'*'*20}" })
+      context 'When operator is "-"' do
+        it 'should return 200 status' do
+          post '/calcs.json', params: { calc: { operator: '-', a: 1, b: 3 } }.to_json, headers: headers
+          expect(response).to have_http_status(200)
+        end
+
+        it 'should create a calc object and return json' do
+          post '/calcs.json', params: { calc: { operator: '-', a: 1, b: 3 } }.to_json, headers: headers
+          expect(json).to eq({ 'result' => "Operation: 1 - 3\nResult: -2\nID: #{Calc.last.id}\nCount: 1\n#{'*'*20}" })
+        end
+      end
+
+      context 'When operator is "*"' do
+        it 'should return 200 status' do
+          post '/calcs.json', params: { calc: { operator: '*', a: 1, b: 3 } }.to_json, headers: headers
+          expect(response).to have_http_status(200)
+        end
+
+        it 'should create a calc object and return json' do
+          post '/calcs.json', params: { calc: { operator: '*', a: 1, b: 3 } }.to_json, headers: headers
+          expect(json).to eq({ 'result' => "Operation: 1 * 3\nResult: 3\nID: #{Calc.last.id}\nCount: 1\n#{'*'*20}" })
+        end
+      end
+
+      context 'When operator is "/"' do
+        it 'should return 200 status' do
+          post '/calcs.json', params: { calc: { operator: '/', a: 4, b: 2 } }.to_json, headers: headers
+          expect(response).to have_http_status(200)
+        end
+
+        it 'should create a calc object and return json' do
+          post '/calcs.json', params: { calc: { operator: '/', a: 4, b: 2 } }.to_json, headers: headers
+          expect(json).to eq({ 'result' => "Operation: 4 / 2\nResult: 2\nID: #{Calc.last.id}\nCount: 1\n#{'*'*20}" })
+        end
       end
     end
   end
