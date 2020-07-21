c7037946a69926d6884a25df979de102362a7563
Victor <viktor.markevich@faceit.com.ua>
2020-07-21 08:39:56 +0300

    Add rspec

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem 'haml-rails', '~> 2.0'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'rspec-rails', '~> 4.0.1'
+  gem 'factory_bot_rails'
 end
 
 group :development do
--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
-require "rails/test_unit/railtie"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -28,5 +28,8 @@ module CalcApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
